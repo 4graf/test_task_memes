b1b56d89fa9a2eba44f86b66950186a7c6a270f0
4graf <senya.nabatov@mail.ru>
2024-08-02 11:51:09 +0300

    Добавление миграций и настроек для базы данных

--- app/settings.py
+++ app/settings.py
@@ -3,6 +3,7 @@
 """
 
 from dotenv import load_dotenv
+from pydantic import PostgresDsn
 from pydantic_settings import BaseSettings
 
 load_dotenv()
@@ -12,7 +13,7 @@ class DatabaseSettings(BaseSettings):
     """
     Настройки для работы с базой данных.
 
-    :cvar postgresql_url: Url подключения к PostgreSQL.
+    :cvar postgres_url: Url подключения к PostgreSQL.
     """
 
-    postgresql_url: str
+    postgres_url: PostgresDsn
