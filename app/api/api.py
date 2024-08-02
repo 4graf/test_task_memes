ac721cb8cfb584cbf928ef061c1f9e8566933a12
4graf <senya.nabatov@mail.ru>
2024-08-02 14:29:39 +0300

    Добавление Docker и docker-compose

--- app/api/api.py
+++ app/api/api.py
@@ -1,3 +1,4 @@
+from app.api.healthcheck.controllers import healthcheck_router
 from app.api.mem.controllers import mem_router
 
-api_routers = [mem_router]
+api_routers = [mem_router, healthcheck_router]
