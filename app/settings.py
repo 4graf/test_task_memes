4ba4a1a0011733da837df2d1f419dff4c6aa9886
4graf <senya.nabatov@mail.ru>
2024-08-14 23:23:37 +0300

    Добавление nginx

--- app/settings.py
+++ app/settings.py
@@ -14,11 +14,14 @@ class DatabaseSettings(BaseSettings):
     Настройки для работы с базой данных.
 
     :cvar postgres_url: Url подключения к PostgreSQL.
+    :cvar s3_storage_url: Url подключения к S3.
+    :cvar s3_access_key_id: Идентификатор ключа S3.
+    :cvar s3_secret_access_key: Секретный ключ S3.
     """
     postgres_url: PostgresDsn
-    s3_storage_url: str
     s3_access_key_id: str
     s3_secret_access_key: str
+    s3_storage_url: str
 
 
 class ImageStorageSettings(BaseSettings):
@@ -41,9 +44,9 @@ class AuthenticationSettings(BaseSettings):
     :cvar refresh_secret_key: Секретный ключ для генерации токенов обновления.
     :cvar refresh_expiration: Время жизни токенов обновления в минутах.
     :cvar jwt_algorithm: Алгоритм кодирования токена.
-    :cvar password_salt: Соль для хеширования пароля пользователя.
-    :cvar iters_hashing: Количество итераций для хеширования пароля пользователя.
-    :cvar hash_algorithm: Алгоритм для хеширования пароля пользователя.
+    :cvar password_salt: Соль для хэширования пароля пользователя.
+    :cvar iters_hashing: Количество итераций для хэширования пароля пользователя.
+    :cvar hash_algorithm: Алгоритм для хэширования пароля пользователя.
     """
 
     access_secret_key: str
